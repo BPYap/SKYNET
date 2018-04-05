711b7b24450e2bfb21e4ab84dbae328b4377d1da
BPYap <32807097+BPYap@users.noreply.github.com>
2018-04-05 23:26:56 +0800

    Minor Changed to zoom and pan animation

--- app/src/main/java/com/skynet/map/Map.java
+++ app/src/main/java/com/skynet/map/Map.java
@@ -119,9 +119,8 @@ public class Map {
     public void setPosition(double latitude, double longitude) {
         LatLong latLong = new LatLong(latitude, longitude);
         byte zoom_level = (byte)((int)max_zoom_level - 1);
-        
-        mapView.getModel().mapViewPosition.setZoomLevel(zoom_level);
-        mapView.getModel().mapViewPosition.animateTo(latLong);
+        MapPosition mapPosition = new MapPosition(latLong, zoom_level);
+        mapView.getModel().mapViewPosition.setMapPosition(mapPosition, true);
     }
 
     public byte getZoomLevelDefault() {
